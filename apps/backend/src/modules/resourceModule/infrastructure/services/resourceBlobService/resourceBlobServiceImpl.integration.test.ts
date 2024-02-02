bada577f3b1fe0157cce98eb604b1bf6c4952a15
Michal Cieslar <michalovskyyy@gmail.com>
2024-02-02 23:41:25 +0100

    add resource blob service tests

--- apps/backend/src/modules/resourceModule/infrastructure/services/resourceBlobService/resourceBlobServiceImpl.integration.test.ts
+++ apps/backend/src/modules/resourceModule/infrastructure/services/resourceBlobService/resourceBlobServiceImpl.integration.test.ts
@@ -3,15 +3,19 @@ import { expect, describe, it, beforeEach, afterEach } from 'vitest';
 
 import { Generator } from '@common/tests';
 
+import { testSymbols } from '../../../../../../tests/container/symbols.js';
 import { TestContainer } from '../../../../../../tests/container/testContainer.js';
 import { type DependencyInjectionContainer } from '../../../../../libs/dependencyInjection/dependencyInjectionContainer.js';
 import { type ResourceBlobService } from '../../../domain/services/resourceBlobService/resourceBlobService.js';
 import { symbols } from '../../../symbols.js';
+import { type S3TestUtils } from '../../../tests/utils/s3TestUtils.js';
 
 describe('ResourceBlobServiceImpl', () => {
   let container: DependencyInjectionContainer;
 
   let resourceBlobService: ResourceBlobService;
+
+  let s3TestUtils: S3TestUtils;
 
   const resourcesDirectory = path.resolve(__dirname, '../../../../../../../resources');
 
@@ -19,17 +23,21 @@ describe('ResourceBlobServiceImpl', () => {
 
   const sampleFileName2 = 'sample_video2.mp4';
 
-  const bucketName = 'resources1';
+  const bucketName = 'resources';
 
   beforeEach(async () => {
     container = TestContainer.create();
 
     resourceBlobService = container.get<ResourceBlobService>(symbols.resourceBlobService);
 
-    await azuriteService.createContainer(containerName);
+    s3TestUtils = container.get<S3TestUtils>(testSymbols.s3TestUtils);
+
+    await s3TestUtils.createBucket(bucketName);
   });
 
-  afterEach(async () => {});
+  afterEach(async () => {
+    await s3TestUtils.deleteBucket(bucketName);
+  });
 
   describe('download', () => {
     it('throws an error - when bucket does not exist', async () => {
@@ -67,6 +75,8 @@ describe('ResourceBlobServiceImpl', () => {
     });
 
     it('downloads a file', async () => {
+      await s3TestUtils.uploadObject(bucketName, sampleFileName1, path.join(resourcesDirectory, sampleFileName1));
+
       const resource = await resourceBlobService.downloadResource({
         bucketName,
         resourceName: sampleFileName1,
@@ -106,6 +116,8 @@ describe('ResourceBlobServiceImpl', () => {
     });
 
     it('returns true - when resource exists', async () => {
+      await s3TestUtils.uploadObject(bucketName, sampleFileName1, path.join(resourcesDirectory, sampleFileName1));
+
       const resourceExists = await resourceBlobService.resourceExists({
         bucketName,
         resourceName: sampleFileName1,
@@ -151,31 +163,29 @@ describe('ResourceBlobServiceImpl', () => {
     });
 
     it('deletes a resource', async () => {
-      const resourceName = Generator.word();
+      await s3TestUtils.uploadObject(bucketName, sampleFileName1, path.join(resourcesDirectory, sampleFileName1));
 
-      await azuriteService.uploadBlob(bucketName, resourceName, path.join(resourcesDirectory, sampleFileName1));
-
-      const existsBefore = await azuriteService.resourceExists(bucketName, resourceName);
+      const existsBefore = await s3TestUtils.objectExists(bucketName, sampleFileName1);
 
       expect(existsBefore).toBe(true);
 
-      await resourceBlobService.deleteBlob({
+      await resourceBlobService.deleteResource({
         bucketName,
-        resourceName,
+        resourceName: sampleFileName1,
       });
 
-      const existsAfter = await azuriteService.resourceExists(bucketName, resourceName);
+      const existsAfter = await s3TestUtils.objectExists(bucketName, sampleFileName1);
 
       expect(existsAfter).toBe(false);
     });
   });
 
-  describe('getBlobsMetadata', () => {
+  describe('getMetadata', () => {
     it('throws an error - when bucket does not exist', async () => {
       const nonExistingBucketName = Generator.word();
 
       try {
-        await resourceBlobService.getBlobsMetadata({
+        await resourceBlobService.getResourcesMetadata({
           bucketName: nonExistingBucketName,
           page: 1,
           pageSize: 10,
@@ -190,7 +200,11 @@ describe('ResourceBlobServiceImpl', () => {
     });
 
     it('returns resources metadata with single page', async () => {
-      const { items, totalPages } = await resourceBlobService.getBlobsMetadata({
+      await s3TestUtils.uploadObject(bucketName, sampleFileName1, path.join(resourcesDirectory, sampleFileName1));
+
+      await s3TestUtils.uploadObject(bucketName, sampleFileName2, path.join(resourcesDirectory, sampleFileName2));
+
+      const { items, totalPages } = await resourceBlobService.getResourcesMetadata({
         bucketName,
         page: 1,
         pageSize: 10,
@@ -208,30 +222,62 @@ describe('ResourceBlobServiceImpl', () => {
     it('returns resources metadata with many pages', async () => {
       const resourceName1 = Generator.word();
 
-      await azuriteService.uploadBlob(bucketName, resourceName1, path.join(resourcesDirectory, sampleFileName1));
-
       const resourceName2 = Generator.word();
 
-      await azuriteService.uploadBlob(bucketName, resourceName2, path.join(resourcesDirectory, sampleFileName1));
+      const resourceName3 = Generator.word();
 
-      const { items, totalPages } = await resourceBlobService.getBlobsMetadata({
+      const resourceName4 = Generator.word();
+
+      await s3TestUtils.uploadObject(bucketName, resourceName1, path.join(resourcesDirectory, sampleFileName1));
+
+      await s3TestUtils.uploadObject(bucketName, resourceName2, path.join(resourcesDirectory, sampleFileName2));
+
+      await s3TestUtils.uploadObject(bucketName, resourceName3, path.join(resourcesDirectory, sampleFileName2));
+
+      await s3TestUtils.uploadObject(bucketName, resourceName4, path.join(resourcesDirectory, sampleFileName2));
+
+      const { items: items1, totalPages: totalPages1 } = await resourceBlobService.getResourcesMetadata({
         bucketName,
         page: 1,
         pageSize: 2,
       });
 
-      expect(items.length).toBe(2);
+      expect(items1.length).toBe(2);
 
-      expect(totalPages).toBe(2);
+      expect(totalPages1).toBe(2);
+
+      const { items: items2, totalPages: totalPages2 } = await resourceBlobService.getResourcesMetadata({
+        bucketName,
+        page: 2,
+        pageSize: 2,
+      });
+
+      expect(items2.length).toBe(2);
+
+      expect(totalPages2).toBe(2);
+
+      const uniqueItems = [...new Set([...items1, ...items2].map((item) => item.name))];
+
+      expect(uniqueItems.length).toBe(4);
+
+      const { items: items3, totalPages: totalPages3 } = await resourceBlobService.getResourcesMetadata({
+        bucketName,
+        page: 3,
+        pageSize: 2,
+      });
+
+      expect(items3.length).toBe(0);
+
+      expect(totalPages3).toBe(2);
     });
   });
 
-  describe('getBlobsNames', () => {
+  describe('getNames', () => {
     it('throws an error - when bucket does not exist', async () => {
       const nonExistingBucketName = Generator.word();
 
       try {
-        await resourceBlobService.getBlobsNames({
+        await resourceBlobService.getResourcesNames({
           bucketName: nonExistingBucketName,
         });
       } catch (error) {
@@ -244,7 +290,11 @@ describe('ResourceBlobServiceImpl', () => {
     });
 
     it('returns resources names', async () => {
-      const resourcesNames = await resourceBlobService.getBlobsNames({
+      await s3TestUtils.uploadObject(bucketName, sampleFileName1, path.join(resourcesDirectory, sampleFileName1));
+
+      await s3TestUtils.uploadObject(bucketName, sampleFileName2, path.join(resourcesDirectory, sampleFileName2));
+
+      const resourcesNames = await resourceBlobService.getResourcesNames({
         bucketName,
       });
 
